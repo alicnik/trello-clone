6c65bb9fedcb32df797e6cebf513d12c1a5607b4
Alex Nicholas <alexander.nicholas@generalassemb.ly>
2021-09-02 10:11:13 +0100

    minor changes

--- src/main/java/com/example/trelloclone/controllers/LabelController.java
+++ src/main/java/com/example/trelloclone/controllers/LabelController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/cards/labels")
 public class LabelController {
 
-    private LabelService labelService;
+    private final LabelService labelService;
 
     @Autowired
     public LabelController(LabelService labelService) {
