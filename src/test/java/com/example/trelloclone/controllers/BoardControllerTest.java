109ff8ba8091df25beafc9a1c6a1163e3d59bc08
Alex Nicholas <me@alexnicholas.dev>
2021-09-03 14:15:19 +0100

    setup

---
+++ src/test/java/com/example/trelloclone/controllers/BoardControllerTest.java
@@ -0,0 +1,35 @@
+package com.example.trelloclone.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@SpringJUnitWebConfig
+class BoardControllerTest {
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup(WebApplicationContext wac) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    void getAllBoards() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/api/v1/boards/");
+        mockMvc.perform(request).andExpect(status().isOk());
+    }
+}
