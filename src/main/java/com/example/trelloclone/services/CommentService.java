9c4ec25e9b6d0b064f9e6b0c695db66298dd99f3
Alex Nicholas <alexander.nicholas@generalassemb.ly>
2021-09-10 11:16:05 +0100

    update tests

--- src/main/java/com/example/trelloclone/services/CommentService.java
+++ src/main/java/com/example/trelloclone/services/CommentService.java
@@ -4,6 +4,8 @@ import com.example.trelloclone.models.Comment;
 import com.example.trelloclone.repositories.CardRepository;
 import com.example.trelloclone.repositories.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
