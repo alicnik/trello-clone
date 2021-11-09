e16cb78904bb2bb920578e560ef5a3697221e1fb
Alex Nicholas <me@alexnicholas.dev>
2021-11-09 11:50:08 +0000

    remove @data annotation and position

--- src/main/java/com/example/trelloclone/models/BoardList.java
+++ src/main/java/com/example/trelloclone/models/BoardList.java
@@ -1,10 +1,7 @@
 package com.example.trelloclone.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -12,9 +9,8 @@ import java.util.List;
 
 @Entity
 @Table(name = "lists")
-@Data
-//@Getter
-//@Setter
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -43,9 +39,6 @@ public class BoardList {
     @OneToMany(mappedBy = "boardList", cascade = CascadeType.ALL)
     @JsonIgnoreProperties("boardList")
     private List<Card> cards;
-
-    @Column(name = "position")
-    private int position;
 
     @Column
     private boolean archived;
